e06881f459a3762123c68d6caeaf7e1355f15381
lorrainesufr0829-pixel <lorrainesufr0829@gmail.com>
2026-08-01 15:24:28 +0000

    34

--- js/supabase.js
+++ js/supabase.js
@@ -8,3 +8,7 @@ window.supabaseClient = window.supabase.createClient(
     "https://uccznppecrkdbgykrucx.supabase.co/",
     "sb_publishable_TWW6H1Xuq8gXp1xgDRKSbA_Gq2aNXwA"
 );
+console.log(
+    "Supabase client:",
+    window.supabaseClient
+);
